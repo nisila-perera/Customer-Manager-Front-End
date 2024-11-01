8b891db5eb71a382280cf8c8f6bd53df869f5735
Nisila Perera <nisilainsitha1@gmail.com>
2024-11-01 12:08:36 +0530

    Initial

---
+++ src/app/page/manage-customers/manage-customers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-customers',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-customers.component.html',
+  styleUrl: './manage-customers.component.css'
+})
+export class ManageCustomersComponent {
+
+}
